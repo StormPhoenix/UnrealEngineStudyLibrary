efb569853a47d66a6fc26b9f991871f8aa5968f6
jiaxianwang <jiaxianwang@tencent.com>
2023-01-31 19:56:08 +0800

    【标准场景资源验证】增加 AssetValidationUtils 类负责蓝图调用。

---
+++ Source/UnrealEngineStudyLibrary/Private/AssetValidationUtils.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AssetValidationUtils.h"
+#include "AssetRegistry/IAssetRegistry.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+#include "Logging/LogMacros.h"
+
+#define LOCTEXT_NAMESPACE "AssetValidation"
+
+DEFINE_LOG_CATEGORY_STATIC(LogAssetValidationUtils, Verbose, All);
+
+namespace AssetValidationTools
+{
+	void SearchAllAssetList()
+	{
+		// @todo 检查 WITH_EDITOR 宏
+		
+		// @todo 测试内容，检查资产搜索是否能在 Android 平台生效
+		FAssetRegistryModule& AssetRegistryModule = FModuleManager::Get().LoadModuleChecked<FAssetRegistryModule>(
+			TEXT("AssetRegistry"));
+		// const FName PackageName = "/UnrealEngineStudyLibrary/Blueprint/BP_ShaderExample.BP_ShaderExample";
+		const FName PackageName = "/UnrealEngineStudyLibrary/Blueprint/BP_ShaderExample";
+		TArray<FName> Dependencies;
+		AssetRegistryModule.Get().GetDependencies(PackageName, Dependencies);
+
+		UE_LOG(LogAssetValidationUtils, Display, TEXT("TestAsset::Dependencies: %d"), Dependencies.Num());
+		for (auto DependsIt = Dependencies.CreateConstIterator(); DependsIt; ++DependsIt)
+		{
+			FString DependencyName = (*DependsIt).ToString();
+			UE_LOG(LogAssetValidationUtils, Display, TEXT("TestAsset::DependencyName: %s"), *DependencyName);
+		}
+		// @todo end
+
+		UE_LOG(LogAssetValidationUtils, Display, TEXT("AssetValidationTools::SearchAllAssetList()"));
+	}
+}
+
+UAssetValidationUtils::UAssetValidationUtils(const FObjectInitializer& ObjInitializer): Super(ObjInitializer)
+{
+}
+
+void UAssetValidationUtils::SearchAssetList(const UObject* WorldContextObject)
+{
+#if WITH_EDITOR
+	AssetValidationTools::SearchAllAssetList();
+#elif
+	AssetValidationTools::SearchAllAssetList();
+#endif
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/UnrealEngineStudyLibrary/Public/AssetValidationUtils.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Engine/Classes/Kismet/BlueprintFunctionLibrary.h"
+#include "AssetValidationUtils.generated.h"
+
+/**
+ * Asset Validation Tools
+ */
+UCLASS(MinimalAPI, meta=(ScriptName="AssetValidationUtils"))
+class UAssetValidationUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_UCLASS_BODY()
+	
+	UFUNCTION(BlueprintCallable, Category = "AssetValidationUtils", meta = (WorldContext = "WorldContextObject"))
+	static void SearchAssetList(const UObject* WorldContextObject);
+};
